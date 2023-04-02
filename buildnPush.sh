e09146b1592728eb3e70831adf00dbed58ddca4e
syukurcode <syukurz87@gmail.com>
2023-04-02 16:18:23 +0800

    docker platform

--- buildnPush.sh
+++ buildnPush.sh
@@ -1,3 +1,4 @@
 docker rmi syukurdocker/thermal_printer
-docker build -t syukurdocker/thermal_printer .
-docker push syukurdocker/thermal_printer
+#docker build -t syukurdocker/thermal_printer .
+#docker push syukurdocker/thermal_printer
+docker buildx build --platform linux/amd64,linux/arm64 --push -t syukurdocker/thermal_printer:latest .
