4ec1742aa47ec77f9842371f31c6ad452a2a9004
syukur <syukur.musa@cloud-connect.asia>
2023-10-16 13:48:09 +0800

    add history

--- index.py
+++ index.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 from escpos import printer
-from flask import Flask, render_template, request, redirect, jsonify
+from flask import Flask, render_template, request, redirect, jsonify, url_for
 import os
 from datetime import datetime
 import json
@@ -22,6 +22,10 @@ migrate.init_app(app, db)
 db.create_all()
 
 machine = printer_machine.Print("/dev/usb/lp0")
+@app.route('/favicon.ico')
+def favicon():
+	return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 @app.route('/print', methods=["POST"])
 def printWrite():
@@ -39,27 +43,49 @@ def printWrite():
 		saveDataToDb(strPrintData)
 		return jsonify({'status': 200, 'text': 'Receipt has been printed'})
 
-@app.route('/', methods=["GET","POST"])
+@app.route('/reprint', methods=["POST"])
+def rePrintWrite():
+        if request.method == 'POST':
+                if not os.path.exists("/dev/usb/lp0"):
+                        return jsonify({'status': 500, 'text': 'Printer not available'})
+
+                printdata = request.get_json()
+                strPrintData = json.dumps(printdata)
+                response = machine.jobs(printdata)
+
+                if response == False:
+                        return jsonify({'status': 500, 'text': 'Data unsucces to print'})
+
+                return jsonify({'status': 200, 'text': 'Receipt has been printed'})
+@app.route('/save', methods=["POST"])
+def save():
+	if request.method == 'POST':
+		savedata = request.get_json()
+		strSaveData = json.dumps(saveData)
+
+		saveDataToDb(strSaveData)
+		return jsonify({'status': 200, 'text': 'Data was saved.'})
+
+@app.route('/', methods=["GET"])
 def index():
 	try:
-		allhistory = RawData.query.all()
 		jenisPerniagaan = JenisPerniagaan.query.all()
 	except Exception as e:
-		allhistory =  ""
+		write("Error: " + e)
 
 	if request.method == 'GET':
-		return render_template("index.html",allhistory=allhistory,printData="",jenisPerniagaan=jenisPerniagaan)
-	if request.method == 'POST':
-		if request.form:
-			historyID = request.form.get('id')
-			try:
-				Data = RawData.query.filter_by(id=historyID).first()
-			except Exception as e:
-				Data = RawData(id=0,tarikh="",data="")
+		return render_template("index.html",jenisPerniagaan=jenisPerniagaan)
 
-			printData = Data.data
-			print(printData)
-			return render_template("home.html",allhistory=allhistory,printData=printData)
+@app.route('/history', methods=["GET"])
+def history():
+	try:
+		allhistory = RawData.query.all()
+	except Exception as e:
+		allhistory = ""
+		writeLogs("Error: " + e)
+
+	if request.method == 'GET':
+		return render_template("history.html",allhistory=allhistory)
 
 def saveDataToDb(text):
 	if not text == "":
